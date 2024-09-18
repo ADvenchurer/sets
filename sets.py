e78e7fc8b297a8427cfccca9d80ee682bb6d31a3
ADvenchurer <adamekwalker@gmail.com>
2024-09-18 18:57:20 +0200

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,62 @@
+set1 = {1,5,2,"hello"}
+print (set1)
+
+list1 = [2,1,3,"hello"]
+
+set2=list1
+
+print (set2)
+
+
+items = ["bananna","apple", "0469713881"]
+items.sort()
+
+print(items)
+
+
+items2 = {1,True,"geography"}
+
+
+###print(items2[1])###
+
+points=10
+seconds=2
+
+score=[points,seconds]
+
+totalScore = score[0]/score[1]
+
+print (totalScore)
+
+blankSet = set()
+
+print(blankSet)
+
+
+newSet = set("my_favourite_colour_is_blue")
+
+print(newSet)
+
+
+numberSet = set("1_5_2_3")
+
+print(numberSet)
+
+random = ["hello","sup","hello"] 
+
+print(random)
+
+coolSet = set(["hello","sup","hello"])
+print(coolSet)
+
+supercoolSet = set([1,5,4,2,6,7,7,7,6,3,2,1,1])
+
+print(supercoolSet)
+
+supercoolSet.add(8)
+
+print(supercoolSet)
+
+numberSet.update([8,8,8,8,8,9,9,10,11,11])
+
+print(numberSet)
